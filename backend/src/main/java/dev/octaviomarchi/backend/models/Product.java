1f1a50555684b037ad474ea7e88109f8355cad30
Octavio Marchi Parisi <octavio.marchi@gmail.com>
2021-09-12 18:10:50 -0300

    produto fine tunning

--- backend/src/main/java/dev/octaviomarchi/backend/models/Product.java
+++ backend/src/main/java/dev/octaviomarchi/backend/models/Product.java
@@ -2,6 +2,7 @@ package dev.octaviomarchi.backend.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.Nullable;
 
@@ -18,14 +19,14 @@ import javax.persistence.Table;
 public class Product {
 
     @Id
-    @Column(name = "COD_PRODUTO", nullable = false, length = 4)
+    @Column(name = "COD_PRODUTO", nullable = false, length = 4, columnDefinition = "char(4)")
     private String codProduto;
 
-    @Column(name = "DES_PRODUTO")
+    @Column(name = "DES_PRODUTO", length = 30, columnDefinition = "varchar(30)")
     @Nullable
     private String desProduto;
 
-    @Column(name = "STA_STATUS")
+    @Column(name = "STA_STATUS", length = 1, columnDefinition = "char(1)")
     @Nullable
     private String staStatus;
 }
